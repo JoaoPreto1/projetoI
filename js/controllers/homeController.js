6626c2d0c32fd591c9366bd5564e0d7db770a00f
JoaoPreto1 <129522013+JoaoPreto1@users.noreply.github.com>
2025-05-29 14:49:59 +0100

    login

--- js/controllers/homeController.js
+++ js/controllers/homeController.js
@@ -1,25 +1,26 @@
 
-import { renderHomePage } from "../views/homeView.html";
-
 export function initHomePage() {
-  document.getElementById("app").innerHTML = renderHomePage();
+  fetch("../views/homeView.html")
+    .then(response => response.text())
+    .then(html => {
+      document.getElementById("app").innerHTML = html;
 
-  window.addEventListener("load", () => {
-    const loader = document.querySelector(".loader-wrapper");
-    loader.style.transition = "opacity 0.5s ease";
-    loader.style.opacity = 0;
-    setTimeout(() => loader.style.display = "none", 500);
-  });
+      const loader = document.querySelector(".loader-wrapper");
+      loader.style.transition = "opacity 0.5s ease";
+      loader.style.opacity = 0;
+      setTimeout(() => loader.style.display = "none", 500);
 
-  document.addEventListener("DOMContentLoaded", () => {
-    const loginButton = document.getElementById("loginButton");
-    const user = JSON.parse(localStorage.getItem("loggedInUser"));
-
-    if (user) {
-      loginButton.outerHTML = `
-        <a id="profileIcon" class="nav-link" href="perfil.html">
-          <img src="user-icon.png" alt="Perfil" style="width: 35px; height: 35px; border-radius: 50%;" loading="lazy" />
-        </a>`;
-    }
-  });
+      const loginButton = document.getElementById("loginButton");
+      const user = JSON.parse(localStorage.getItem("loggedInUser"));
+  
+      if (user && loginButton) {
+        loginButton.outerHTML = `
+          <a id="profileIcon" class="nav-link" href="perfil.html">
+            <img src="user-icon.png" alt="Perfil" style="width: 35px; height: 35px; border-radius: 50%;" loading="lazy" />
+          </a>`;
+      }
+    })
+    .catch(error => {
+      console.error("Erro ao carregar a home page:", error);
+    });
 }
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import { setupLoginForm } from './controllers/loginController.js';
 import { initAdminUser } from './models/userModel.js';
-import { initHomePage } from "../controllers/homeController.js";
+import { initHomePage } from "./controllers/homeController.js";
 
 
 document.addEventListener('DOMContentLoaded', () => {
